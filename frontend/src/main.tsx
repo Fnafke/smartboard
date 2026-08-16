4f847a18e9447f452c650ffb0cd520a3a4b92aaa
Fnafke <elinbachvarov@gmail.com>
2026-08-16 19:09:54 +0200

    Added proper react routing and removed the app file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { AuthProvider } from './components/context/AuthProvider.tsx'
 import { ThemeProvider } from './components/context/ThemeProvider.tsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import HomePage from './Pages/Homepage/index.tsx'
+import LoginPage from './Pages/Loginpage/index.tsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/login' element={<LoginPage />} />
+    </>
+  )
+)
 
 createRoot(document.getElementById('root')!).render(
   <AuthProvider>
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
           <StrictMode>
-            <App />
+            <RouterProvider router={router} />
           </StrictMode>
     </ThemeProvider>
   </AuthProvider>
